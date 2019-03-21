88a006106e32dfa25ed412edc3d677d0e3df4542
shonnahalli <shonnahalli@riverbed.com>
2019-03-21 05:02:04 -0700

    Added reverse and reverse(k group of elements) Long description
    BugID:
    ReviewID:
    CC:
    Reviewed-By:
    Approved-By:
    
    List of files modified/added/renamed/deleted

--- list.cc
+++ list.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<list>
 
 using namespace std;
 
@@ -99,6 +100,92 @@ public:
 		head = t;
 		return T(NULL);
 	}
+
+    void reverse(void) {
+		struct list_<T>* prev = NULL;
+		struct list_<T>* curr = NULL;
+		struct list_<T>* next = NULL;
+
+		curr = head;
+		if (head == NULL) {
+			cout << "List is empty nothing to reverse!" << endl;
+			return;
+		}
+		next = curr->next;
+		while (curr) {
+			curr->next = prev;
+			prev = curr;
+			curr = next;
+			if(next)
+			next = next->next;
+		}
+		head = prev;
+		return;
+	}
+
+    /*
+	 This function reverses the group of k elements in the list
+	 assume the k=3 and list contains (head)10->20->30->40->50->60->NULL
+	 the resultant list should look like:
+	 (head)30->20->10->60->50->40->NULL
+
+	 steps to solution:
+	 reverse only k elements and connect it`s head with tail of its previous k reversed elements
+	 lets take a small analysis from the above considered list:
+	 1. 1st group of 3 list 30->20->10->NULL
+	 2. 2nd group of 3 list 60->50->40->NULL
+	 3. connect 10->60 which means the list would look like: 
+	 30->20->10->60->50->40->NULL
+
+	 How do we achive it?
+	 1. put all the elements that needs to be connected i.e 10, 40 in to a seperate list list.push_back(10, 40)
+	 2. once size of list reaches 2, i.e sorting 2 k grouped elements is done connect them
+
+
+	 This also means if the group size i.e k = 1 then list remains the same!
+	 why so? reversing 1 group of elements results in the same 1 element! 
+	*/
+	void reverse(int k) {
+		struct list_<T>* prev = NULL;
+		struct list_<T>* curr = NULL;
+		struct list_<T>* next = NULL;
+		
+		auto set_head = true;
+		list<struct list_<T>*> connect_nodes;
+		
+		curr = head;
+		if (head == NULL) {
+			cout << "List is empty nothing to reverse!" << endl;
+			return;
+		}
+		next = curr->next;
+		while (curr) {
+			auto count = 1;
+			prev = NULL;
+			connect_nodes.push_back(curr);
+			while ((count <= k) && curr) {
+				curr->next = prev;
+				prev = curr;
+				curr = next;
+				if (next)
+					next = next->next;
+				count++;
+			}
+			if (set_head) {
+				head = prev;
+				set_head = false;
+			}
+			if (connect_nodes.size() == 2) {
+				auto connect = connect_nodes.front();
+				connect->next = prev;
+				connect_nodes.pop_front();
+			}
+		}
+		auto connect = connect_nodes.front();
+		connect->next = curr;
+		connect_nodes.pop_front();
+		return;
+	}
 };
 
 int main()
@@ -110,10 +197,11 @@ int main()
 	list.push_back(40);
 	list.print_list();
 
-	list.pop_back();
-	list.pop_back();
-	list.pop_back();
-	list.pop_back();
+	//list.pop_back();
+	//list.pop_back();
+	//list.pop_back();
+	//list.pop_back();
+    list.reverse(3);
 	list.print_list();
 	cout << "Done!" << endl;
     
